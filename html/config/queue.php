6fd893ac79377b67af7c03e6ecd7fb7b0ca38789
bxack911 <bxack911@gmail.com>
2020-10-04 16:41:41 +0300

    Setting up rabbitmq

--- html/config/queue.php
+++ html/config/queue.php
@@ -1,7 +1,38 @@
 <?php
 
 return [
+    'rabbitmq' => [
 
+        'driver' => 'rabbitmq',
+        'queue' => env('RABBITMQ_QUEUE', 'default'),
+        'connection' => PhpAmqpLib\Connection\AMQPLazyConnection::class,
+
+        'hosts' => [
+            [
+                'host' => env('RABBITMQ_HOST', 'rabbitmq'),
+                'port' => env('RABBITMQ_PORT', 5672),
+                'user' => env('RABBITMQ_USER', 'bxack911'),
+                'password' => env('RABBITMQ_PASSWORD', 'wormixnik911'),
+                'vhost' => env('RABBITMQ_VHOST', '/'),
+            ],
+        ],
+
+        'options' => [
+            'ssl_options' => [
+                'cafile' => env('RABBITMQ_SSL_CAFILE', null),
+                'local_cert' => env('RABBITMQ_SSL_LOCALCERT', null),
+                'local_key' => env('RABBITMQ_SSL_LOCALKEY', null),
+                'verify_peer' => env('RABBITMQ_SSL_VERIFY_PEER', true),
+                'passphrase' => env('RABBITMQ_SSL_PASSPHRASE', null),
+            ],
+        ],
+
+        /*
+         * Set to "horizon" if you wish to use Laravel Horizon.
+         */
+        'worker' => env('RABBITMQ_WORKER', 'default'),
+
+    ],
     /*
     |--------------------------------------------------------------------------
     | Default Queue Connection Name
@@ -31,7 +62,7 @@ return [
     'connections' => [
 
         'sync' => [
-            'driver' => 'sync',
+            'driver' => 'rabbitmq',
         ],
 
         'database' => [
