723239244712c1f7fb724a834f3dcf83b7749b9a
bxack911 <bxack911@gmail.com>
2020-10-04 19:12:02 +0300

    Setting up rabbitmq

--- html/config/queue.php
+++ html/config/queue.php
@@ -1,38 +1,7 @@
 <?php
 
 return [
-    'rabbitmq' => [
 
-        'driver' => 'rabbitmq',
-        'queue' => env('RABBITMQ_QUEUE', 'default'),
-        'connection' => PhpAmqpLib\Connection\AMQPLazyConnection::class,
-
-        'hosts' => [
-            [
-                'host' => env('RABBITMQ_HOST', 'rabbitmq'),
-                'port' => env('RABBITMQ_PORT', 5672),
-                'user' => env('RABBITMQ_USER', 'bxack911'),
-                'password' => env('RABBITMQ_PASSWORD', 'wormixnik911'),
-                'vhost' => env('RABBITMQ_VHOST', '/'),
-            ],
-        ],
-
-        'options' => [
-            'ssl_options' => [
-                'cafile' => env('RABBITMQ_SSL_CAFILE', null),
-                'local_cert' => env('RABBITMQ_SSL_LOCALCERT', null),
-                'local_key' => env('RABBITMQ_SSL_LOCALKEY', null),
-                'verify_peer' => env('RABBITMQ_SSL_VERIFY_PEER', true),
-                'passphrase' => env('RABBITMQ_SSL_PASSPHRASE', null),
-            ],
-        ],
-
-        /*
-         * Set to "horizon" if you wish to use Laravel Horizon.
-         */
-        'worker' => env('RABBITMQ_WORKER', 'default'),
-
-    ],
     /*
     |--------------------------------------------------------------------------
     | Default Queue Connection Name
@@ -60,44 +29,105 @@ return [
     */
 
     'connections' => [
+        'rabbitmq' => [
 
-        'sync' => [
             'driver' => 'rabbitmq',
-        ],
 
-        'database' => [
-            'driver' => 'database',
-            'table' => 'jobs',
-            'queue' => 'default',
-            'retry_after' => 90,
-        ],
+            /*
+         * Set to "horizon" if you wish to use Laravel Horizon.
+         */
+            'worker' => env('RABBITMQ_WORKER', 'default'),
 
-        'beanstalkd' => [
-            'driver' => 'beanstalkd',
-            'host' => 'localhost',
-            'queue' => 'default',
-            'retry_after' => 90,
-            'block_for' => 0,
-        ],
+            'dsn' => env('RABBITMQ_DSN', null),
 
-        'sqs' => [
-            'driver' => 'sqs',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'prefix' => env('SQS_PREFIX', 'https://sqs.us-east-1.amazonaws.com/your-account-id'),
-            'queue' => env('SQS_QUEUE', 'your-queue-name'),
-            'suffix' => env('SQS_SUFFIX'),
-            'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-        ],
+            /*
+         * Could be one a class that implements \Interop\Amqp\AmqpConnectionFactory for example:
+         *  - \EnqueueAmqpExt\AmqpConnectionFactory if you install enqueue/amqp-ext
+         *  - \EnqueueAmqpLib\AmqpConnectionFactory if you install enqueue/amqp-lib
+         *  - \EnqueueAmqpBunny\AmqpConnectionFactory if you install enqueue/amqp-bunny
+         */
 
-        'redis' => [
-            'driver' => 'redis',
-            'connection' => 'default',
-            'queue' => env('REDIS_QUEUE', 'default'),
-            'retry_after' => 90,
-            'block_for' => null,
-        ],
+            'factory_class' => \Enqueue\AmqpBunny\AmqpConnectionFactory::class,
 
+            'host' => env('RABBITMQ_HOST', 'rabbitmq'),
+            'port' => env('RABBITMQ_PORT', 5672),
+
+            'vhost' => env('RABBITMQ_VHOST', '/'),
+            'login' => env('RABBITMQ_LOGIN', 'bxack911'),
+            'password' => env('RABBITMQ_PASSWORD', 'wormixnik911'),
+
+            'queue' => env('RABBITMQ_QUEUE', 'default'),
+
+            'options' => [
+
+                'exchange' => [
+
+                    'name' => env('RABBITMQ_EXCHANGE_NAME'),
+
+                    /*
+                 * Determine if exchange should be created if it does not exist.
+                 */
+
+                    'declare' => env('RABBITMQ_EXCHANGE_DECLARE', true),
+
+                    /*
+                 * Read more about possible values at https://www.rabbitmq.com/tutorials/amqp-concepts.html
+                 */
+
+                    'type' => env('RABBITMQ_EXCHANGE_TYPE', \Interop\Amqp\AmqpTopic::TYPE_DIRECT),
+                    'passive' => env('RABBITMQ_EXCHANGE_PASSIVE', false),
+                    'durable' => env('RABBITMQ_EXCHANGE_DURABLE', true),
+                    'auto_delete' => env('RABBITMQ_EXCHANGE_AUTODELETE', false),
+                    'arguments' => env('RABBITMQ_EXCHANGE_ARGUMENTS'),
+                ],
+
+                'queue' => [
+
+                    /*
+                 * Determine if queue should be created if it does not exist.
+                 */
+
+                    'declare' => env('RABBITMQ_QUEUE_DECLARE', true),
+
+                    /*
+                 * Determine if queue should be binded to the exchange created.
+                 */
+
+                    'bind' => env('RABBITMQ_QUEUE_DECLARE_BIND', true),
+
+                    /*
+                 * Read more about possible values at https://www.rabbitmq.com/tutorials/amqp-concepts.html
+                 */
+
+                    'passive' => env('RABBITMQ_QUEUE_PASSIVE', false),
+                    'durable' => env('RABBITMQ_QUEUE_DURABLE', true),
+                    'exclusive' => env('RABBITMQ_QUEUE_EXCLUSIVE', false),
+                    'auto_delete' => env('RABBITMQ_QUEUE_AUTODELETE', false),
+                    'arguments' => env('RABBITMQ_QUEUE_ARGUMENTS'),
+                ],
+            ],
+
+            /*
+         * Determine the number of seconds to sleep if there's an error communicating with rabbitmq
+         * If set to false, it'll throw an exception rather than doing the sleep for X seconds.
+         */
+
+            'sleep_on_error' => env('RABBITMQ_ERROR_SLEEP', 5),
+
+            /*
+         * Optional SSL params if an SSL connection is used
+         * Using an SSL connection will also require to configure your RabbitMQ to enable SSL. More details can be founds here: https://www.rabbitmq.com/ssl.html
+         */
+
+            'ssl_params' => [
+                'ssl_on' => env('RABBITMQ_SSL', false),
+                'cafile' => env('RABBITMQ_SSL_CAFILE', null),
+                'local_cert' => env('RABBITMQ_SSL_LOCALCERT', null),
+                'local_key' => env('RABBITMQ_SSL_LOCALKEY', null),
+                'verify_peer' => env('RABBITMQ_SSL_VERIFY_PEER', true),
+                'passphrase' => env('RABBITMQ_SSL_PASSPHRASE', null),
+            ],
+        ],
     ],
 
     /*
